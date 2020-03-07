d3f2bfb1b5c6b6a70913c914bb5b83fb9999112b
孙志岩 <61651364+sunzhiyan@users.noreply.github.com>
2020-03-07 09:41:53 +0800

    Add files via upload
    测试文件

---
+++ test.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+'''
+@File : test.py
+@Time : 2020/03/06 22:05:50
+@Author : xdbcb8 
+@Version : 1.0
+@Contact : xdbcb8@qq.com
+@WebSite : www.xdbcb8.com
+'''
+
+# here put the import lib
+print("abcd")
