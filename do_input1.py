f7e5947ded2f81ed16694adb0c9b6ffe3555a023
孙志岩 <61651364+sunzhiyan@users.noreply.github.com>
2020-03-07 10:48:25 +0800

    输入输出

---
+++ do_input1.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+'''
+@File : 输入.py
+@Time : 2020/03/04 08:26:25
+@Author : xdbcb8 
+@Version : 1.0
+@Contact : xdbcb8@qq.com
+@WebSite : www.xdbcb8.com
+'''
+
+# here put the import lib1
+x=int(input("请输入购买苹果的斤数："))
+y=int(input("请输入苹果的价格:"))
+m=x*y
+print("您应付款:")
+print(m)
