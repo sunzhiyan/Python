a7c3a5cdcb5ade6efc0119c60da16e5e8beb614a
孙志岩 <61651364+sunzhiyan@users.noreply.github.com>
2020-03-07 10:12:48 +0800

    字典

---
+++ the_dict.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+'''
+@File : 字典1.py
+@Time : 2020/03/03 22:29:15
+@Author : xdbcb8 
+@Version : 1.0
+@Contact : xdbcb8@qq.com
+@WebSite : www.xdbcb8.com
+'''
+
+# here put the import lib
+dict1={'num' : '01','name' : '李明','class' : '软件1班','age' : 18}
+print(dict1)
+dict2={'01' : '李明','02' : '张三','03' : '小李','04' : '小红','05' : '王五'}
+print(dict2['01'])
+print(dict2)
