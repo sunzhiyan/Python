69b51c2b1259a7b86af2d0e5bdeb653b4baa3cbd
孙志岩 <61651364+sunzhiyan@users.noreply.github.com>
2020-03-07 09:42:19 +0800

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+'''
+@File : hello.py
+@Time : 2020/03/07 08:40:40
+@Author : xdbcb8 
+@Version : 1.0
+@Contact : xdbcb8@qq.com
+@WebSite : www.xdbcb8.com
+'''
+
+# here put the import lib
+print("我爱中国！")
