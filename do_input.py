38210cdd27ab66328863f12b89f327863f35a8d7
孙志岩 <61651364+sunzhiyan@users.noreply.github.com>
2020-03-07 10:45:24 +0800

    输入

---
+++ do_input.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+'''
+@File : do_input.py
+@Time : 2020/03/07 10:43:38
+@Author : xdbcb8 
+@Version : 1.0
+@Contact : xdbcb8@qq.com
+@WebSite : www.xdbcb8.com
+'''
+
+# here put the import lib
+print("输入数据:")
+x=input()
+xlist=x.split(" ")
+print(xlist)
+print(tuple(xlist))
