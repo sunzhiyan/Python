a93fd9c194a55c7e79db4ab631b44274dbca23d6
孙志岩 <61651364+sunzhiyan@users.noreply.github.com>
2020-03-07 09:52:43 +0800

    元组

---
+++ the_tuple.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+'''
+@File : 元组.py
+@Time : 2020/03/03 22:21:56
+@Author : xdbcb8 
+@Version : 1.0
+@Contact : xdbcb8@qq.com
+@WebSite : www.xdbcb8.com
+'''
+
+# here put the import lib
+tup=(54,65,98,89,100)
+print(len(tup))
+print(tup+tup)
+print(54 in tup)
+print(tup)
+print(tup[3:5])
+print(min(tup))
+print(max(tup))
