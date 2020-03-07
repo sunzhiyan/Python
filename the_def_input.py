926004e0f89da789ae178de5e24739aba325fe2d
孙志岩 <61651364+sunzhiyan@users.noreply.github.com>
2020-03-07 11:26:01 +0800

    函数输入

---
+++ the_def_input.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+'''
+@File : 练习.py
+@Time : 2020/03/03 21:41:03
+@Author : xdbcb8 
+@Version : 1.0
+@Contact : xdbcb8@qq.com
+@WebSite : www.xdbcb8.com
+'''
+
+# here put the import lib
+
+def myfun():
+    x=int(input("请输入购买苹果的斤数："))
+    y=int(input("请输入苹果的价格:"))
+    m=x*y
+    print("您应付款：")
+    print(m)
+myfun()
