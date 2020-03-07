9b0de7cb99680001b1dafb3733eb81624c47b173
孙志岩 <61651364+sunzhiyan@users.noreply.github.com>
2020-03-07 09:50:48 +0800

    列表

---
+++ the_list.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+'''
+@File : 列表.py
+@Time : 2020/03/03 22:09:13
+@Author : xdbcb8 
+@Version : 1.0
+@Contact : xdbcb8@qq.com
+@WebSite : www.xdbcb8.com
+'''
+
+# here put the import lib
+list=[56,23,89,99,56,45,87,56,65,100]
+print(max(list))
+print(min(list))
+print(sum(list))
+print(sum(list)/len(list))
