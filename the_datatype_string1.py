d6812b2cb462d8294c503d3e2b4bfc60559b00f1
孙志岩 <61651364+sunzhiyan@users.noreply.github.com>
2020-03-07 09:46:46 +0800

    字符串1

---
+++ the_datatype_string1.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+'''
+@File : 字符串查找.py
+@Time : 2020/03/03 21:56:10
+@Author : xdbcb8 
+@Version : 1.0
+@Contact : xdbcb8@qq.com
+@WebSite : www.xdbcb8.com
+'''
+
+# here put the import lib
+str='1234567899876543210'
+print(str.find('123'))
+print(str.find('32141'))
+b=str.replace('123456','sfsdada')
+print(b)
+print(len(str))
+print(len(b))
