7fb54143b90ac53ef3ca379e3d616e5f574000df
孙志岩 <61651364+sunzhiyan@users.noreply.github.com>
2020-03-07 10:20:17 +0800

    字典1

---
+++ the_dict1.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+'''
+@File : the_dict1.py
+@Time : 2020/03/07 10:14:09
+@Author : xdbcb8 
+@Version : 1.0
+@Contact : xdbcb8@qq.com
+@WebSite : www.xdbcb8.com
+'''
+
+# here put the import lib
+dict = {'Name': 'Runoob', 'Age': 7, 'Class': 'First'}
+print(dict)
+dict['Age']=18
+print(dict)
+del dict['Name']
+print(dict)
+dict['Name']='李明'
+print(dict)
