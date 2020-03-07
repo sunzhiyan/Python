7a604bda050b40cfe9cc90d89eb537f65627ff8c
sunzhiyan <477589421@qq.com>
2020-03-07 09:11:07 +0800

    第一次提交

---
+++ the_datatype_string.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+'''
+@File : 字符串.py
+@Time : 2020/03/03 21:44:12
+@Author : xdbcb8 
+@Version : 1.0
+@Contact : xdbcb8@qq.com
+@WebSite : www.xdbcb8.com
+'''
+
+# here put the import lib
+word = "字符串"
+sentence = "这是一个句子。"
+paragraph ="""这是一个段落，
+可以有多行组成"""
+print(word)
+print(sentence)
+print(paragraph)
+str='runoob'
+print(str[0:-1])
+print(str*2)
+print('hello\nrunoob')
+print(r'hello\nrunoob')
